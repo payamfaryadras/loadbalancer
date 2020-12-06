de2fc61740278b0959b3cedcbfb5b27d878c4c64
payam <payam.ff@gmail.com>
2020-12-06 09:52:07 +0330

    add first version of loadbalancer with express-docker:)

---
+++ app/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 5000
+const name = process.env.name || 'World!';
+app.get('/', (req, res) => {
+    res.send(`Hello ${name}!`);
+});
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+})
